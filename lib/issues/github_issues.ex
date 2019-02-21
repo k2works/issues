43fab9f55f781380a57ed2550c62b0770bf18c2e
k2works <kakimomokuri@gmail.com>
2019-02-21 13:36:23 +0900

    chore: タスク:ロギングの追加

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,5 +1,9 @@
 defmodule Issues.GithubIssues do
+
+  require Logger
+
   @user_agent [{"User-agent", "Elixir dave@proagprog.com"}]
+
   @github_url Application.get_env(:issues, :github_url)
 
   def issues_url(user, project) do
@@ -7,6 +11,7 @@ defmodule Issues.GithubIssues do
   end
 
   def fetch(user, project) do
+    Logger.info "Fetching user #{user}'s project #{project}"
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -17,10 +22,13 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({:ok, %{status_code: 200, body: body}}) do
+    Logger.info "Successful response"
+    Logger.debug fn -> inspect(body) end
     {:ok, Poison.Parser.parse!(body)}
   end
 
-  def handle_response({___, %{status_code: ___, body: body}}) do
+  def handle_response({_, %{status_code: status, body: body}}) do
+    Logger.error "Error #{status} returned"
     {:error, Poison.Parser.parse!(body)}
   end
 end
