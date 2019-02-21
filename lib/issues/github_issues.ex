c341021e97c336041a8af9864e30cc2572289dd4
k2works <kakimomokuri@gmail.com>
2019-02-21 12:59:31 +0900

    fix: issues_urlタイポ修正

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -3,7 +3,7 @@ defmodule Issues.GithubIssues do
   @github_url Application.get_env(:issues, :github_url)
 
   def issues_url(user, project) do
-    "#{@github_url}/repose/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
   def fetch(user, project) do
