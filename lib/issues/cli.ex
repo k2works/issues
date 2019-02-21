5da6f90787a159a2316de06c7b403882fa9e37db
k2works <kakimomokuri@gmail.com>
2019-02-21 11:25:03 +0900

    feat: 変換:データを並び替える

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -42,6 +42,11 @@ defmodule Issues.CLI do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response
     |> convert_to_list_of_maps
+    |> sort_into_ascending_order
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues, fn i1, i2 -> i1["created_at"] <= i2["created_at"] end
   end
 
   def decode_response({:ok, body}), do: body
